55f5423ac19dd8628a6c6a0c50337109256d0250
Josephine Qiu <josephineqiu0@gmail.com>
2017-11-19 22:26:43 +0000

    destroy button

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
  get 'todo/show/:id', to: 'todo#show'
  get 'todo/edit/:id', to: 'todo#edit'
  get 'todo/update/:id', to: 'todo#update'
+ get 'todo/destroy/:id', to: 'todo#destroy'
  end
