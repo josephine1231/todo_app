666d2daef80f7afe92e78c29f2f4d1db3aafbaef
Josephine Qiu <josephineqiu0@gmail.com>
2017-11-19 19:49:16 +0000

    add and wire create

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -6,4 +6,11 @@ class TodoController <ApplicationController
     end
     def new
     end
+    def create
+        t = Todo.new
+        t.description=params['description']
+        t.pomodoro_estimate=params['pomodoro_estimate']
+        t.save
+        redirect_to "/todo/show/#{ t.id }"
+    end
 end
